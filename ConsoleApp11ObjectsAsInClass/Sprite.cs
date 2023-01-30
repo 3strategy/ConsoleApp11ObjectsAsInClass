cf52164c74faa7484f585ea08a639f84aabc2550
Guy Siedes <3strategy@gmail.com>
2023-01-30 15:10:32 +0200

    Add project files.

---
+++ ConsoleApp11ObjectsAsInClass/Sprite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11ObjectsAsInClass
+{
+  public class Sprite : Point
+  {
+    public Sprite(int x, int y) : base(x, y)
+    {
+
+    }
+    public void Draw()
+    {
+      Console.SetCursorPosition(x, y);
+      Console.WriteLine("X");
+    }
+
+  }
+}
