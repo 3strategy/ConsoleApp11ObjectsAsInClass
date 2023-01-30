a086a09cb1d57aebb21bfbb18ef24827f2b06b36
Guy Siedes <3strategy@gmail.com>
2023-01-30 16:22:11 +0200

    sprite moving

--- ConsoleApp11ObjectsAsInClass/Program.cs
+++ ConsoleApp11ObjectsAsInClass/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace ConsoleApp11ObjectsAsInClass
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp11ObjectsAsInClass
 {
-  internal class Program
+  public class Program
   {
+    static int speed = 1000;
+    static Sprite player = new Sprite(5, 5);
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      while (true)
+      {
+        while (!Console.KeyAvailable)
+        {  
+          player.Move();
+          Thread.Sleep(speed);
+        }
+        var key = Console.ReadKey(true).Key; //key press handling
+        if (key == ConsoleKey.Escape)
+          break; //exit game
+        else
+          HandleKey(key);
+      }
+
+    }
+    private static void HandleKey(ConsoleKey key)
+    {
+      //if (key == ConsoleKey.N)
+      //  GameReInit();
+      //control game speed.
+      if (key == ConsoleKey.Add)
+        speed = speed * 8 / 10;
+      else if (key == ConsoleKey.Subtract)
+        speed = speed * 10 / 8;
+      else
+      {
+        //foreach (var player in players)
+        //  player.HandleKeys(key);
+        player.HandleKeys(key);
+      }
     }
   }
 }
--- ConsoleApp11ObjectsAsInClass/Sprite.cs
+++ ConsoleApp11ObjectsAsInClass/Sprite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace ConsoleApp11ObjectsAsInClass
 {
   public class Sprite : Point
   {
+    private int dX;
+    private int dY;
+
     public Sprite(int x, int y) : base(x, y)
     {
 
@@ -18,5 +22,30 @@ namespace ConsoleApp11ObjectsAsInClass
       Console.WriteLine("X");
     }
 
+    public void HandleKeys(ConsoleKey key)
+    {
+      if (key == ConsoleKey.RightArrow)
+        dX++;
+      else if (key == ConsoleKey.LeftArrow)
+        dX--;
+      else if (key == ConsoleKey.DownArrow)
+        dY++;
+      else if (key == ConsoleKey.UpArrow) dY--;
+
+    }
+
+    public void Erase()
+    {
+      Console.ForegroundColor = ConsoleColor.Black;
+      Draw();
+      Console.ForegroundColor = ConsoleColor.White;
+    }
+    public void Move()
+    {
+      Erase();
+      x += dX;
+      y += dY;
+      Draw();
+    }
   }
 }
